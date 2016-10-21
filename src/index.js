e62409e404e24a9a02f7c88e40e9f898434aa720
Jirka Endršt <Jirka Endrt>
2016-10-21 17:38:48 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router'
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
