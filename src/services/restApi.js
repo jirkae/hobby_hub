af6bea98d49671d7de0a22eb24ddf2832e8e7cfe
Jirka Endršt <Jirka Endrt>
2016-11-14 14:09:54 +0100

    hotfix

--- src/services/restApi.js
+++ src/services/restApi.js
@@ -1,6 +1,7 @@
 import axios, { CancelToken } from 'axios';
 
 const BASE_URL = "http://localhost:3000/api/";
+//const BASE_URL = "http://dev.backend.team03.vse.handson.pro/api/";
 
 export const postRegister = (props) => {
   return axios.post(`${BASE_URL}AuthUsers`,
