17a32f6a1ac7ea196e06da14d9ee66a687726b62
martin.machacek <martin.machacek@inventi.cz>
2016-11-15 22:37:00 +0100

    Zaveden thunk, zbývá dořešit reducery a zaregistrovat komponenty jako posluchačě změn stavu

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,6 +1,6 @@
 import {combineReducers} from 'redux';
 
-import modalReducer from './modalWindowReducer';
+import modalReducer from './modalReducer';
 import userReducer from './userReducer';
 
 export default combineReducers({
