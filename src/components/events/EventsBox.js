f75e6ee0c00e10f7986f8fdebcd0088e2cf00bd8
Martin Bůna <martin.buna@gmail.com>
2016-12-01 07:33:26 +0100

    vyhledavani z landing page

--- src/components/events/EventsBox.js
+++ src/components/events/EventsBox.js
@@ -2,56 +2,58 @@ import React, {Component} from 'react';
 import {Grid, Row, Col, Glyphicon} from "react-bootstrap";
 import EventsBoxItem from "./EventsBoxItem.js";
 import {baseUrl} from '../../services/restApi.js';
+import { getEvents } from '../../services/restApi';
 
 export default class EventsBox extends Component {
 
-    state = {
-        events: [],
-        url: ""
-    };
+  state = {
+      city: this.props.city,
+      tags: this.props.tags,
+      events: []
+  };
 
-    componentDidMount()
-    {
-        const url = baseUrl() + 'events';
-        fetch(url).then(r => r.json()).then(json => {
-            this.setState({events: json})
-        }).catch(e => console.log("Error"));
-    }
+  componentDidMount()
+  {
+    var params = {city: this.state.city, tags: this.state.tags};
+    var events = getEvents(params).then(response => {
+      this.setState({events: response.data.events});
+    });
+  }
 
-    getEventsBoxItems(events)
-    {
-        var items = events.map((data) => {
-            return (<EventsBoxItem event={data}/>);
-        })
-        return items;
-    }
+  getEventsBoxItems()
+  {
+      var items = this.state.events.map((data) => {
+          return (<EventsBoxItem event={data} key={data.id}/>);
+      })
+      return items;
+  }
 
     render()
     {
-        const {events} = this.state;
         return (
             <Grid className="container-top-margin">
-                <Row>
-                    <Col sm={12} className="page-content col-thin-right">
-                        <Col lg={12} className="content-box">
-                            <Row className="row-featured row-featured-category">
-                                <Col lg={12} className="box-title no-border">
-                                    <div className="inner">
-                                        <h2>
-                                            <span>Akce</span>
-                                            <a href="#" className="sell-your-item">
-                                                Zobrazit více akcí <Glyphicon glyph="th-list"></Glyphicon>
-                                            </a>
-                                        </h2>
-                                    </div>
-                                </Col>
-                                <div className="adds-wrapper">
+              <Row>
+                <Col sm={12} className="page-content col-thin-right">
+                  <Col lg={12} className="content-box">
+                    <Row className="row-featured row-featured-category">
+                      <Col lg={12} className="box-title no-border">
+                        <div className="inner">
+                          <h2>
+                            <span>Akce</span>
+                            <a href="#" className="sell-your-item">
+                              Zobrazit více akcí
+                              <Glyphicon glyph="th-list"></Glyphicon>
+                            </a>
+                          </h2>
+                        </div>
+                      </Col>
+                      <div className="adds-wrapper">
 
-                                    {this.getEventsBoxItems(events)}
+                        {this.getEventsBoxItems()}
 
-                                </div>
-                                <div className="tab-box save-search-bar text-center">
-                                    <a className="text-uppercase" href="#">
+                      </div>
+                      <div className="tab-box save-search-bar text-center">
+                        <a className="text-uppercase" href="#">
                                         <i className="icon-briefcase"></i>
                                         Zobrazit všechny akce
                                     </a>
--- src/pages/EventsListPage.js
+++ src/pages/EventsListPage.js
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
 import {Grid, Col, FormControl, Button} from "react-bootstrap";
 import EventsBox from "../components/events/EventsBox.js";
+import { getEvents } from '../services/restApi';
+import { getLatestEvents } from '../services/restApi';
 
 export default class EventsListPage extends Component {
-componentDidMount () {
-  console.log(this.props.location.query);
+
+constructor(props)
+{
+  super(props);
+
+  this.state = {
+    city: this.props.location.query["city"],
+    tags: this.props.location.query["tags"]
+  };
 }
 
   render() {
-    const { query } = this.props.location;
+    const {events} = this.state;
+
     return (
         <div>
           <div className="search-row-wrapper landingBackgroundEvents">
             <Grid className="text-center">
               <Col sm={3}>
-                <FormControl className="keyword" placeholder="sport, koníček, událost" type="text" value={query.a===undefined ? '' : query.a}/>
+                <FormControl className="keyword" placeholder="sport, koníček, událost" type="text"/>
               </Col>
               <Col sm={3}>
                 <FormControl componentClass="select">
@@ -37,7 +47,8 @@ componentDidMount () {
               </Col>
             </Grid>
           </div>
-          <EventsBox/>
+
+          <EventsBox city={this.state.city} tags={this.state.tags} />
         </div>
     );
   }
