61c4790038bcbf49148cf55b96287fc5d1759e35
Martin Bůna <martin.buna@gmail.com>
2017-01-21 19:45:13 +0100

    cas u komentaru

--- src/Event/components/EventComments.js
+++ src/Event/components/EventComments.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import moment from 'moment';
+import { Row, Col } from 'react-bootstrap';
 
 import { addComment } from './../actions/eventActionCreators';
 
@@ -26,22 +28,26 @@ class EventComments extends Component {
         const {comments} = this.props;
 
         return (
-            <div>
-                <h4 className="text-uppercase">Komentáře</h4>
-                <div className="form-group">
-                    <textarea className="form-control" value={this.state.newComment} onChange={(e) => { this.setState({ newComment: e.target.value }); } }></textarea>
-                    <button className="btn btn-xs btn-success" onClick={this.handleAddComment}>Přidat komentář</button>
-                </div>
-                {comments.map((item) => (
-                    <div key={item.id} className="media">
-                        <div className="media-body">
-                            <h4 className="media-heading">{item.user.firstName} {item.user.lastName}</h4>
-                            {item.text}
-                        </div>
+            <Row>
+                <Col xs="12">
+                    <h4 className="text-uppercase">Komentáře</h4>
+                    <div className="form-group">
+                        <textarea className="form-control" value={this.state.newComment} onChange={(e) => { this.setState({ newComment: e.target.value }); } }></textarea>
+                        <button className="btn btn-xs btn-success" onClick={this.handleAddComment}>Přidat komentář</button>
                     </div>
-                ))}
-
-            </div>
+                    {comments.map((item) => (
+                        <div key={item.id} className="media">
+                            <div className="media-body">
+                                <div>
+                                    <strong>{item.user.firstName} {item.user.lastName}</strong>
+                                    {moment(item.dateCreated).fromNow()}
+                                </div>
+                                {item.text}
+                            </div>
+                        </div>
+                    ))}
+                </Col>
+            </Row>
         );
     }
 }
